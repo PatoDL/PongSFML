ebf1bd1500b38772d6fab061d444543cf76a726c
unknown <pehuen-testa@hotmail.com>
2018-11-09 12:52:44 -0300

    agregado el tilemap

--- Pong/src/main.cpp
+++ Pong/src/main.cpp
@@ -1,24 +1,41 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
+#include "STP/TMXLoader.hpp"
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 1600), "STP Example");
+	tmx::TileMap map("res/atr.tmx");
 
-	while (window.isOpen())
-	{
+	map.ShowObjects(); // Display all the layer objects.
+
+	//map.GetLayer("World").visible = false; // Hide a Layer named World
+
+										   
+
+	sf::View view1(sf::FloatRect(200.f, 200.f,800.f,600.f));
+	sf::View view2(sf::Vector2f(400.f, 300.f), sf::Vector2f(800.f, 600.f));
+
+	window.setView(view2);
+
+
+	while (window.isOpen()) {    // Start the game loop
+		// Process events
 		sf::Event event;
-		while (window.pollEvent(event))
-		{
+		while (window.pollEvent(event)) {
+			// Close window : exit
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
+		sf::View currentview = window.getView();
+
+		// Clear screen
 		window.clear();
-		window.draw(shape);
+		// Draw the map
+		window.draw(window);
+		// Update the window
 		window.display();
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
